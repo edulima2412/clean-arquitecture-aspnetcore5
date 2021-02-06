b933ad58694eab0155ffd5945933575e39d1057c
Eduardo Lima <edulima2412@gmail.com>
2021-02-06 11:48:49 -0300

    criado services e mapeamento feito com automapper

--- CleanArch.Domain/Interfaces/IProductRepository.cs
+++ CleanArch.Domain/Interfaces/IProductRepository.cs
@@ -1,10 +1,15 @@
 ﻿using CleanArch.Domain.Entities;
 using System.Collections.Generic;
+using System.Threading.Tasks;
 
 namespace CleanArch.Domain.Interfaces
 {
     public interface IProductRepository
     {
-        IEnumerable<Product> GetProducts();
+        Task<IEnumerable<Product>> GetProducts();
+        Task<Product> GetById(int? id);
+        void Add(Product product);
+        void Update(Product product);
+        void Remove(Product product);
     }
 }
